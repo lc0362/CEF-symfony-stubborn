eb8326dfc112cee6ddc608d2d3927e3b4717debf
laurac <laurac0362@gmail.com>
2025-03-17 14:18:06 +0100

    add category and product in bdd

---
+++ src/Entity/Product.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enum\SizeEnum;
+use App\Repository\ProductRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 15)]
+    private ?string $name = null;
+
+    #[ORM\Column(enumType: SizeEnum::class)]
+    private ?SizeEnum $size = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 6, scale: 2)]
+    private ?string $price = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSize(): ?SizeEnum
+    {
+        return $this->size;
+    }
+
+    public function setSize(SizeEnum $size): static
+    {
+        $this->size = $size;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(string $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
---
+++ src/Enum/SizeEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enum;
+
+enum SizeEnum: string
+{
+    case XS = 'XS';
+    case S = 'S';
+    case M = 'M';
+    case L = 'L';
+    case XL = 'XL';
+}
