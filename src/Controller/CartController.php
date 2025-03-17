ba7591701625753855bc78471616dd8dfcfab70c
laurac <laurac0362@gmail.com>
2025-03-17 23:11:54 +0100

    style headr eet footer sur ttes els pages

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use App\Entity\Product;
+use App\Repository\ProductRepository;
 
 
 final class CartController extends AbstractController
