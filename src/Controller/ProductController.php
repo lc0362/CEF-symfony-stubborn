7205072cc0c56b76aaf2f8d10ddf71e878dacf05
laurac <laurac0362@gmail.com>
2025-03-20 16:52:10 +0100

    modif style global et ajour fiche produit

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -12,7 +12,7 @@ final class ProductController extends AbstractController
     #[Route('/product/{id}', name: 'product_show')]
     public function show(Product $product): Response
     {
-        return $this->render('product/show.html.twig', [
+        return $this->render('product/index.html.twig', [
             'product' => $product,
         ]);
     }
