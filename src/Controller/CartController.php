25f315c9414677c6ebb3111ce353e478bd6f1d51
laurac <laurac0362@gmail.com>
2025-03-21 12:15:23 +0100

    start cart

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -4,40 +4,33 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Product;
 use App\Repository\ProductRepository;
 
-
-final class CartController extends AbstractController
+class CartController extends AbstractController
 {
-    #[Route('/cart/add/{id}', name: 'cart_add', methods: ['POST'])]
-    public function addToCart(Product $product, SessionInterface $session): Response
-    {
-        $cart = $session->get('cart', []);
-        $cart[$product->getId()] = ($cart[$product->getId()] ?? 0) + 1;
-
-        $session->set('cart', $cart);
-
-        return $this->redirectToRoute('cart_show');
-    }
     #[Route('/cart', name: 'cart_show')]
     public function showCart(SessionInterface $session, ProductRepository $productRepository): Response
     {
+        // Récupérer le panier de la session
         $cart = $session->get('cart', []);
         $cartProducts = [];
 
-        foreach ($cart as $id => $quantity) {
+        // Pour chaque produit dans le panier, on récupère les informations nécessaires
+        foreach ($cart as $id => $item) {
             $cartProducts[] = [
                 'product' => $productRepository->find($id),
-                'quantity' => $quantity,
+                'quantity' => $item['quantity'],
+                'price' => $item['price'],
             ];
         }
 
+        // Afficher la vue du panier avec les produits ajoutés
         return $this->render('cart/index.html.twig', [
             'cartProducts' => $cartProducts,
         ]);
     }
-
+    
 }
