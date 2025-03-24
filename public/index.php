9d19622bee14d0d0e6b447c7248780ca0e1e355e
laurac <laurac0362@gmail.com>
2025-03-24 17:28:06 +0100

    modif pour déploiement

--- public/index.php
+++ public/index.php
@@ -1,9 +1,17 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+// Chargement du .env uniquement en local si nécessaire
+if ((isset($_SERVER['APP_ENV']) && $_SERVER['APP_ENV'] === 'dev') || (isset($_ENV['APP_ENV']) && $_ENV['APP_ENV'] === 'dev')) {
+    if (file_exists(dirname(__DIR__).'/.env')) {
+        (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+    }
+}
+
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    return new Kernel($context['APP_ENV'] ?? 'dev', (bool) ($context['APP_DEBUG'] ?? true));
 };
