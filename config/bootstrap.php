9d37af9f281547a15646a0184a4a8ab041949019
laurac <laurac0362@gmail.com>
2025-03-24 23:34:22 +0100

    fix bootstrap et index

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,10 +4,6 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-if (!isset($_SERVER['APP_ENV'])) {
-    if (file_exists(dirname(__DIR__) . '/.env')) {
-        (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
-    } else {
-        $_SERVER['APP_ENV'] = 'prod';
-    }
+if (!isset($_ENV['APP_ENV']) && file_exists(dirname(__DIR__) . '/.env')) {
+    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 }
--- public/index.php
+++ public/index.php
@@ -2,15 +2,11 @@
 
 use App\Kernel;
 
-putenv('APP_ENV=prod');
-putenv('APP_DEBUG=0');
-
-
 require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    $env = $_SERVER['APP_ENV'] ?? 'prod';
-    $debug = (bool) ($_SERVER['APP_DEBUG'] ?? false);
+    $env = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'prod';
+    $debug = (bool) ($_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? false);
 
     return new Kernel($env, $debug);
 };
