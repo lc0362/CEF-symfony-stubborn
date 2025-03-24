b74132656c37d06a6a27984e3b751d9c83dcbf4b
laurac <laurac0362@gmail.com>
2025-03-24 18:13:24 +0100

    modif pour déploiement

--- public/index.php
+++ public/index.php
@@ -5,8 +5,8 @@ use App\Kernel;
 require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel(
-        $_ENV['APP_ENV'] ?? $_SERVER['APP_ENV'] ?? 'prod',
-        (bool) ($_ENV['APP_DEBUG'] ?? $_SERVER['APP_DEBUG'] ?? false)
-    );
+    $env = $_SERVER['APP_ENV'] ?? 'prod';
+    $debug = (bool) ($_SERVER['APP_DEBUG'] ?? false);
+
+    return new Kernel($env, $debug);
 };
