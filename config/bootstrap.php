88151bdc9f4de4862902f4dfff696ef10c8e0cc3
laurac <laurac0362@gmail.com>
2025-03-24 23:28:47 +0100

    fix bootstrap

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,8 +4,10 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-if (!isset($_SERVER['APP_ENV']) && !isset($_ENV['APP_ENV'])) {
+if (!isset($_SERVER['APP_ENV'])) {
     if (file_exists(dirname(__DIR__) . '/.env')) {
-        (new Dotenv())->loadEnv(dirname(__DIR__) . '/.env');
+        (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
+    } else {
+        $_SERVER['APP_ENV'] = 'prod';
     }
 }
