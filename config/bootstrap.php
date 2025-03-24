6156fcbe074a53aefd17af8102c09a06c3320da7
laurac <laurac0362@gmail.com>
2025-03-24 23:11:44 +0100

    modif pour déploiement

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,6 +4,6 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-if ($_SERVER['APP_ENV'] === 'dev' && file_exists(dirname(__DIR__) . '/.env')) {
+if (!isset($_SERVER['APP_ENV']) && file_exists(dirname(__DIR__) . '/.env')) {
     (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 }
