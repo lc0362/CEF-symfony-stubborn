2df87b7529e3810753f49780de0dd8075bb3078f
laurac <laurac0362@gmail.com>
2025-03-25 14:43:54 +0100

    tryyyYYYyy

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,4 @@
 <?php
 
 use Symfony\Component\Dotenv\Dotenv;
-
-require dirname(__DIR__).'/vendor/autoload.php';
-
-if (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-}
+require dirname(__DIR__) . '/vendor/autoload.php';
