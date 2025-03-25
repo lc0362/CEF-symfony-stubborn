5beb1be191456912a3e15ce0d89ea16d2bcd213e
laurac <laurac0362@gmail.com>
2025-03-25 14:51:10 +0100

    tryyyYYYyy

--- public/index.php
+++ public/index.php
@@ -1,13 +1,7 @@
 <?php
 use App\Kernel;
-use Symfony\Component\Dotenv\Dotenv;
-
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-if (file_exists(dirname(__DIR__).'/.env')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-}
-
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    return new Kernel($_ENV['APP_ENV'], (bool) $_ENV['APP_DEBUG']);
 };
